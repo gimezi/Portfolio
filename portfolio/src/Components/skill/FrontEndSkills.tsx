a477f2d6c6aaf5565ff04fb467964109becbcfbd
yeji <yeji11077@gmail.com>
2024-07-04 15:16:00 +0900

    [fix] build 오류나는거 주석처리

--- portfolio/src/Components/skill/FrontEndSkills.tsx
+++ portfolio/src/Components/skill/FrontEndSkills.tsx
@@ -1,63 +1,63 @@
-type skillType = {
-    name: string,
-    image:string,
-    projects: number[]
-}
+// type skillType = {
+//     name: string,
+//     image:string,
+//     projects: number[]
+// }
 
 // 프로젝트 번호: 노션에 있는 번호순대로
 function FrontEndSkills() {
-    const FrontSkills:skillType[] = [
-        {
-            name:'JavaScript',
-            image: '',
-            projects: [2, 3]
-        },
-        {
-            name:'TypeScript',
-            image: '',
-            projects: [0, 1]
-        },
-        {
-            name:'HTML',
-            image: '',
-            projects: []
-        },
-        {
-            name:'CSS',
-            image: '',
-            projects: []
-        },
-        {
-            name: 'React',
-            image: '/logo/React.png',
-            projects: [0, 1, 2]
-        },
-        {
-            name:'Redux',
-            image: '',
-            projects: [0, 1, 2]
-        },
-        {
-            name:'Vue.js',
-            image: '',
-            projects: [3]
-        },
-        {
-            name:'Three.js',
-            image: '',
-            projects: [1]
-        },
-        {
-            name:'Tailwind CSS',
-            image: '',
-            projects: [2, 3]
-        },
-        {
-            name:'Styled Component',
-            image: '',
-            projects: [2, 3]
-        },
-    ]
+    // const FrontSkills:skillType[] = [
+    //     {
+    //         name:'JavaScript',
+    //         image: '',
+    //         projects: [2, 3]
+    //     },
+    //     {
+    //         name:'TypeScript',
+    //         image: '',
+    //         projects: [0, 1]
+    //     },
+    //     {
+    //         name:'HTML',
+    //         image: '',
+    //         projects: []
+    //     },
+    //     {
+    //         name:'CSS',
+    //         image: '',
+    //         projects: []
+    //     },
+    //     {
+    //         name: 'React',
+    //         image: '/logo/React.png',
+    //         projects: [0, 1, 2]
+    //     },
+    //     {
+    //         name:'Redux',
+    //         image: '',
+    //         projects: [0, 1, 2]
+    //     },
+    //     {
+    //         name:'Vue.js',
+    //         image: '',
+    //         projects: [3]
+    //     },
+    //     {
+    //         name:'Three.js',
+    //         image: '',
+    //         projects: [1]
+    //     },
+    //     {
+    //         name:'Tailwind CSS',
+    //         image: '',
+    //         projects: [2, 3]
+    //     },
+    //     {
+    //         name:'Styled Component',
+    //         image: '',
+    //         projects: [2, 3]
+    //     },
+    // ]
     return(
         <div className="w-[98%] h-[98%] bg-gray-100/50 rounded">
             <p className="text-lg p-3" style={{fontFamily:'SEBANG_Gothic_Bold'}}>FrontEnd</p>
